91710264c07b0a16beeaafe7c504d7a42164cfb9
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2025-04-02 13:00:09 +0200

    seperated server and database connection

--- database/index.ts
+++ database/index.ts
@@ -1,1 +1,16 @@
 import mongoose from "mongoose";
+
+const mongoString = process.env.DATABASE_URL as string;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(mongoString);
+        console.log("Database connected");
+    } catch (error) {
+        console.error("Database connection error:", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
+
--- index.ts
+++ index.ts
@@ -1,32 +1,24 @@
 import dotenv from 'dotenv';
 import app from './config/app';
 import { Request, Response } from 'express';
-
-import mongoose from 'mongoose';
-
-const mongoString = process.env.DATABASE_URL as string;
-mongoose.connect(mongoString);
-
-const database = mongoose.connection;
-
-database.on('error', (error) => {
-    console.log(error)
-})
-
-database.once('connected', () => {
-    console.log('Database Connected');
-})
-
+import connectDB from './database';
 dotenv.config();
+
 
 const port = process.env.PORT || 8000;
 
-app.get('/test', (req: Request, res: Response): void => {
-    res.status(200).json({
-        message: 'server is running and the test endpoint is working! yipieee',
-    });
-});
+const startServer = async () => {
+    await connectDB();
 
-app.listen(port, () => {
-    console.log(`Server is running at https://localhost:${port}`);
-});
+    app.listen(port, () => {
+        console.log(`Server is running at https://localhost:${port}`);
+    });
+
+    app.get('/test', (req: Request, res: Response): void => {
+        res.status(200).json({
+            message: 'server is running and the test endpoint is working! yipieee',
+        });
+    });
+}
+
+startServer();
