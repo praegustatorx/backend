26e2edefc43279123a6094e828a7fcf9cc09cae7
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2025-04-03 16:16:11 +0200

    Update nutritional_information.ts

--- models/nutritional_information.ts
+++ models/nutritional_information.ts
@@ -1,7 +1,45 @@
 // TODO: Add abstractions 
 export type Nutritions = {
-    calories: number;
-    protein: number;
-    fat: number;
-    carbohydrates: number;
+    portion: NutrientAmount;
+    calories: [NutrientAmount, NutrientAmount];
+    protein: NutrientAmount;
+    fat: NutrientAmount;
+    carbohydrates: NutrientAmount;
 };
+
+export const createNutritions = (
+    portion: NutrientAmount = createNutrientAmount(100, NutrientUnit.GRAM),
+    calories_amount: number = 0,
+    protein_amount: number = 0,
+    fat_amount: number = 0,
+    carbohydrates_amount: number = 0
+): Nutritions => {
+    let calories = createNutrientAmount(calories_amount, NutrientUnit.CALORIES);
+    let kilojoules = createNutrientAmount(calories_amount * 4.184, NutrientUnit.KILOJOULES);
+    let protein = createNutrientAmount(protein_amount, NutrientUnit.GRAM);
+    let fat = createNutrientAmount(fat_amount, NutrientUnit.GRAM);
+    let carbohydrates = createNutrientAmount(carbohydrates_amount, NutrientUnit.GRAM);
+
+    return { portion, calories: [kilojoules, calories], protein, fat, carbohydrates };
+};
+
+/**
+ * Nutritional Amount per Portion
+ */
+export type NutrientAmount = {
+    amount: number;
+    unit: NutrientUnit;
+}
+
+export const createNutrientAmount = (amount: number, unit: NutrientUnit): NutrientAmount => {
+    return { amount, unit };
+};
+
+/**
+ *  Nutritional Measuring Units
+ */
+export enum NutrientUnit {
+    GRAM = "g",
+    CALORIES = "kcal",
+    KILOJOULES = "kj",
+}
