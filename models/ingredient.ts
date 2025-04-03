8f9f3b6d9bd1e56a994a87bed8955eb129dca98c
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2025-04-03 18:16:37 +0200

    Create ingredient.ts

---
+++ models/ingredient.ts
@@ -0,0 +1,54 @@
+import { Option } from "ts-results-es";
+import { Nutritions } from "./nutritional_information";
+
+/**
+ * Representing abstracted ingredient definition. 
+ */
+type GenericIngredient = {
+    id: string;
+    name: string;
+}
+
+/**
+ * Representing the quantity of an ingredient.
+*/
+type Measurement = {
+    quantity: number;
+    unit: Unit;
+}
+// TODO: Create a type PantryUnit, which will be type limited to unit: Unit.MILILITER | Unit.GRAM
+
+// TODO: Add more or less measurements if needed
+enum Unit {
+    MILLILITER = "milliliter",
+    LITER = "liter",
+    GRAM = "gram",
+    MILLIGRAM = "milligram",
+    KILOGRAM = "kilogram",
+    CUP = "cup",
+    TABLESPOON = "tablespoon",
+    TEASPOON = "teaspoon",
+    PINCH = "pinch",
+}
+
+// TODO: keep ingredient's measurements only in mililiter and gram.
+/** 
+ * Representing an ingredient in the pantry.
+ * The pantry ingredient is a concrete example of a generic ingredient with a brand and additional information.
+*/
+type PantryIngredient = {
+    id: string;
+    brand: Option<string>;
+    genericId: string;
+    quantity: Option<Measurement>;
+    nutrition: Nutritions;
+}
+
+/**
+ * Representing a recipe ingredient.
+ * The recipe ingredient is a concrete example of a generic ingredient with quantity.
+*/
+type RecipeIngredient = {
+    genericId: string;
+    quantity: Option<Measurement>;
+}
