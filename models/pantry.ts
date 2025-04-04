9fc4b823cb3773d1931d397ac5450b54ed00206c
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2025-04-04 09:20:06 +0200

    Refactor Pantry to use Map

--- models/pantry.ts
+++ models/pantry.ts
@@ -1,17 +1,22 @@
+import { Err, Ok, Result } from "ts-results-es";
 import { PantryIngredient } from "./ingredient";
 
 export type Pantry = {
-    ingredients: PantryIngredient[];
+    ingredients: Map<string, PantryIngredient>;
 }
 
 export const createPantry = (ingredients: PantryIngredient[] = []): Pantry => {
-    return { ingredients };
+    const ingredientsMap = new Map<string, PantryIngredient>();
+    ingredients.forEach(ingredient => {
+        ingredientsMap.set(ingredient.id, ingredient);
+    });
+    return { ingredients: ingredientsMap };
 };
 
 export const addIngredient = (pantry: Pantry, ingredient: PantryIngredient): void => {
-    pantry.ingredients.push(ingredient);
+    pantry.ingredients.set(ingredient.id, ingredient);
 };
 
-export const removeIngredient = (pantry: Pantry, ingredientId: string): void => {
-    pantry.ingredients = pantry.ingredients.filter(ingredient => ingredient.id !== ingredientId);
+export const removeIngredient = (pantry: Pantry, ingredientId: string): Result<void, void> => {
+    return pantry.ingredients.delete(ingredientId) ? Ok(undefined) : Err(undefined);
 };
