ebb294106d4f01d97b2709ccbb0486c071c27b07
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2025-04-05 22:46:08 +0200

    Added & tested Blacklist

--- models/preferences.ts
+++ models/preferences.ts
@@ -1,13 +1,15 @@
 import { Option, Err, Ok, Result, None } from "ts-results-es";
+import { GenericIngredient } from "./ingredient";
 
 // ----- Preferences -----
 export type Preferences = {
   allergies: Option<Allergy[]>;
   diets: Option<Diet[]>;
+  blacklist: Option<Blacklist>;
 };
 
-export const createPreferences = (allergies: Option<Allergy[]> = None, diets: Option<Diet[]> = None): Preferences => {
-  return { allergies, diets };
+export const createPreferences = (allergies: Option<Allergy[]> = None, diets: Option<Diet[]> = None, blacklist: Option<Blacklist> = None): Preferences => {
+  return { allergies, diets, blacklist };
 };
 
 // ----- Diet -----
@@ -46,3 +48,35 @@ export enum Allergy {
   Sesame = 'Sesame',
   Other = 'Other',
 }
+
+// ----- Blacklist -----
+// TODO: implement with Map if frequent queries are required
+/**
+ * Represents a blacklist of ingredients that the user doesn't like in addition to other set preferences.
+ */
+export type Blacklist = {
+  items: GenericIngredient[];
+}
+
+export const CreateBlacklist = (): Blacklist => {
+  return {
+    items: []
+  };
+};
+
+export const addToBlacklist = (blacklist: Blacklist, ingredient: GenericIngredient): Blacklist => {
+  // Check if ingredient is already in blacklist to avoid duplicates
+  if (!isBlacklisted(blacklist, ingredient.id)) {
+    blacklist.items.push(ingredient);
+  }
+  return blacklist;
+};
+
+export const removeFromBlacklist = (blacklist: Blacklist, ingredientId: string): Blacklist => {
+  blacklist.items = blacklist.items.filter(item => item.id !== ingredientId);
+  return blacklist;
+};
+
+export const isBlacklisted = (blacklist: Blacklist, ingredientId: string): boolean => {
+  return blacklist.items.some(item => item.id === ingredientId);
+};
