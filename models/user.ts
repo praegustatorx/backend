fc561acccb7bd35d9cb3d5f6a3c5a5b33a9bce9e
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2025-04-13 14:03:31 +0300

    feat(authorization+authentication)

---
+++ models/user.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+//hash the password before saving it
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const User = model("userModel", userSchema);
+
+export default User;
--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -1,37 +1,73 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
+import User from "../models/user";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.post('/login', (req: Request, res: Response): void => {
+router.get("/test", (req: Request, res: Response): void => {
+  console.log("Hello World");
+  res.status(200).json({
+    message: "Server is running and the test endpoint is working!",
+  });
+});
 
-    const { email, password } = req.body;
+router.post("/register", async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
 
+  console.log("name", name);
+  console.log("email", email);
+  console.log("password", password);
 
-    console.log('email:', email);
-    console.log('password', password);
-    if (typeof email !== 'string' || typeof password !== 'string') {
-        res.status(400).json({ message: 'Invalid email or password' });
-        return;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      res.status(409).json({ message: "User already exists" });
+      return;
     }
 
-    const modifiedEmail = `${email.toUpperCase()} ACTUALLY`;
-    const modifiedPassword = password.toUpperCase();
+    const newUser = new User({ name, email, password });
+    await newUser.save();
 
-    res.json({
-        email: modifiedEmail,
-        password: modifiedPassword,
+    res.status(201).json({
+      message: "User registered successfully",
     });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error registering user",
+    });
+  }
 });
 
-router.get('/test', (req: Request, res: Response): void => {
-    console.log('Hello World');
-    res.status(200).json({
-        message: 'Server is running and the test endpoint is working!',
-    });
-});
+router.post("/login", async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
 
-router.post('/signup', (req: Request, res: Response): void => {
-    //
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      console.log("User not found");
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      console.log("Invalid credentials");
+      res.status(401).json({ message: "Invalid credentials" });
+      return;
+    }
+
+    const token = jwt.sign(
+      { userId: user._id.toString() },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ token, user }); //TODO remove password from user object
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
 });
 
 export default router;
