bd08259b8de1f2cc4e6f521ff8aae3518f5c9226
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2025-04-16 20:15:41 +0300

    Added route support for Gemini

--- config/app.ts
+++ config/app.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 
 import authRoter from '../routes/authRoute';
 import fileUploadRouter from '../routes/fileUploadRoute';
+import chatRoute from '../routes/chatRoute';
 
 dotenv.config();
 
@@ -14,5 +15,7 @@ app.use(express.json());
 
 app.use('/auth', authRoter);
 app.use('/file', fileUploadRouter);
+app.use('/chat', chatRoute);
+console.log("Chat route loaded successfully");
 
 export default app;
