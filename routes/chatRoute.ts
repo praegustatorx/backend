5233bb02a72d565b368d63fc3719d2a29d132fac
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2025-04-25 11:55:12 +0200

    updated chat post

--- routes/chatRoute.ts
+++ routes/chatRoute.ts
@@ -1,18 +1,18 @@
-import express from 'express';
-import { Request, Response } from 'express';
-import { AskGemini } from '../chatbot/gemini';
+import express from "express";
+import { Request, Response } from "express";
+import { AskGemini } from "../chatbot/gemini";
 
 const router = express.Router();
 
-router.post('/', async (req: Request, res: Response) => {
-    const { id, message } = req.body;
-    const response = await AskGemini(id, message);
-    if (response.isErr()) {
-        res.status(500).json({ error: response.unwrapErr().message });
-    }
-    const inner = response.unwrap();
-    const result = inner.isSome() ? inner.unwrap() : "No response received.";
-    res.send(result);
+router.post("/", async (req: Request, res: Response) => {
+  const { id, message } = req.body;
+  const response = await AskGemini(id, message);
+  if (response.isErr()) {
+    res.status(500).json({ error: response.unwrapErr().message });
+  }
+  const inner = response.unwrap();
+  const result = inner.isSome() ? inner.unwrap() : "No response received.";
+  res.status(200).send({ message: result });
 });
 
 export default router;
