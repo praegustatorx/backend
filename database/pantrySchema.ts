23a7d14621771bf97d1345379e847c46647cd3f7
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2025-04-27 22:05:34 +0200

    Moved Schemas from pantry to ingredients

--- database/pantrySchema.ts
+++ database/pantrySchema.ts
@@ -3,37 +3,9 @@ import { Pantry, createPantry } from '../models/pantry';
 import { CreatePantryIngredient, PantryIngredient, ExpDate, Unit, CreateMeasurement } from '../models/ingredient';
 import { None, Option, Some } from 'ts-results-es';
 import { createNutritions, NutrientUnit } from '../models/nutritional_information';
+import { PantryIngredientSchema } from './ingredientsSchema';
 
-// Schema for nutrient amount
-const NutrientAmountSchema = new Schema({
-    amount: { type: Number, required: true },
-    unit: { type: String, required: true }
-}, { _id: false });
 
-// Schema for nutritions
-const NutritionsSchema = new Schema({
-    portion: NutrientAmountSchema,
-    calories: [NutrientAmountSchema],
-    protein: NutrientAmountSchema,
-    fat: NutrientAmountSchema,
-    carbohydrates: NutrientAmountSchema
-}, { _id: false });
-
-// Schema for measurement
-const MeasurementSchema = new Schema({
-    quantity: { type: Number, required: true },
-    unit: { type: String, required: true }
-}, { _id: false });
-
-// Schema for PantryIngredient
-const PantryIngredientSchema = new Schema({
-    id: { type: String, required: true },
-    brand: { type: String, required: false },
-    genericId: { type: String, required: true },
-    quantity: MeasurementSchema,
-    nutrition: NutritionsSchema,
-    expiration_date: { type: Date, required: false }
-});
 
 // Interface for the Pantry document
 export interface PantryDocument extends Document {
