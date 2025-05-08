f3b58c0fe527d8034d5b1404879a820a803f44c0
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2025-05-08 21:23:21 +0200

    Replaced old domain with Dto

--- models/pantry.ts
+++ models/pantry.ts
@@ -1,40 +1,15 @@
-import { Err, Ok, Result } from "ts-results-es";
-import { CreateExpDate, ExpDate, isIngredientExpired, PantryIngredient } from "./ingredient";
+import { DbPantryIngredient } from "./ingredient";
 
-export type Pantry = {
-    ingredients: Map<string, PantryIngredient>;
+export type PantryDTO = {
+    userId: string;
+    ingredients: DbPantryIngredient[];
 }
 
-export const createPantry = (ingredients: PantryIngredient[] = []): Pantry => {
-    const ingredientsMap = new Map<string, PantryIngredient>();
-    ingredients.forEach(ingredient => {
-        ingredientsMap.set(ingredient.id, ingredient);
-    });
-    return { ingredients: ingredientsMap };
+export const createPantryDto = (userId: string, ingredients: DbPantryIngredient[]): PantryDTO => {
+    return {
+        userId,
+        ingredients
+    };
 };
 
-export const addIngredient = (pantry: Pantry, ingredient: PantryIngredient): void => {
-    pantry.ingredients.set(ingredient.id, ingredient);
-};
 
-export const removeIngredient = (pantry: Pantry, ingredientId: string): Result<void, void> => {
-    return pantry.ingredients.delete(ingredientId) ? Ok(undefined) : Err(undefined);
-};
-
-export const isEmpty = (pantry: Pantry): boolean => {
-    return pantry.ingredients.size === 0;
-};
-
-/**
- * A generator function that yields expired ingredients from the pantry.
- * @param pantry The pantry to check for expired ingredients.
- * @param date The date to compare against. Defaults to the current date.
- * @returns An iterable iterator of expired PantryIngredient objects.
- */
-export const getExpiredIngredients = function* (pantry: Pantry, date: ExpDate = CreateExpDate()): IterableIterator<PantryIngredient> {
-    for (const ingredient of pantry.ingredients.values()) {
-        if (isIngredientExpired(ingredient, date)) {
-            yield ingredient;
-        }
-    }
-};
