e1c9f31a7e590614a247bcea06b849860d37771a
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2025-05-13 22:32:09 +0200

    Refactored code on calling model api

--- services/ModelServices.ts
+++ services/ModelServices.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import FormData from 'form-data';
 import fs from 'fs';
+import { Result, Ok, Err } from 'ts-results-es';
 
 // Define interfaces for the expected responses
 interface PredictResponse {
@@ -12,22 +13,25 @@ interface OcrResponse {
     extracted_text: string;
 }
 
-export const predictProduct = async (file: Express.Multer.File): Promise<PredictResponse | undefined> => {
+export const predictProduct = async (file: Express.Multer.File): Promise<Result<PredictResponse, Error>> => {
     console.warn(`Calling Python API for predict with file: ${file.originalname}`);
     const formData = new FormData();
     formData.append('file', fs.createReadStream(file.path), file.originalname);
 
     try {
-        const response = await axios.post<PredictResponse>('http://fastapi-app:8000/predict/', formData, {
+        const response = await axios.post<PredictResponse>('http://localhost:8000/predict/', formData, {
             headers: {
                 ...formData.getHeaders()
             }
         });
         console.log('Python server response for /predict:', response.data);
-        return response.data;
+        return Ok(response.data);
     } catch (error) {
         console.error('Error calling /predict endpoint:', error);
-        return undefined;
+        if (error instanceof Error) {
+            return Err(error);
+        }
+        return Err(new Error('An unknown error occurred'));
     } finally {
         if (file.path) {
             fs.unlink(file.path, (err) => {
@@ -39,7 +43,7 @@ export const predictProduct = async (file: Express.Multer.File): Promise<Predict
     }
 };
 
-export const ocrProduct = async (file: Express.Multer.File): Promise<OcrResponse | undefined> => {
+export const ocrProduct = async (file: Express.Multer.File): Promise<Result<OcrResponse, Error>> => {
     console.warn(`Calling Python API for ocr with file: ${file.originalname}`);
     const formData = new FormData();
     formData.append('file', fs.createReadStream(file.path), file.originalname);
@@ -51,10 +55,13 @@ export const ocrProduct = async (file: Express.Multer.File): Promise<OcrResponse
             }
         });
         console.log('Python server response for /ocr:', response.data);
-        return response.data;
+        return Ok(response.data);
     } catch (error) {
         console.error('Error calling /ocr endpoint:', error);
-        return undefined;
+        if (error instanceof Error) {
+            return Err(error);
+        }
+        return Err(new Error('An unknown error occurred'));
     } finally {
         if (file.path) {
             fs.unlink(file.path, (err) => {
